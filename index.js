5ce6a0a2ed9e35e2a9595b528f86dcd6d7a6d9f7
evgeniia-korolova <jkod@ukr.net>
2023-09-04 15:40:30 +0300

    feat : add js

--- index.js
+++ index.js
@@ -1,118 +1,196 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('burger').addEventListener('click', function () {
-    document.querySelector('header').classList.toggle('open');
-})
-} )
-
+    document.querySelector('.header').classList.toggle('open');
+  });
+});
 
 // Закрыть меню при нажатии на Esc
 window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        // Действие при клике
-        document.querySelector(".header").classList.remove("open")
-    }
+  if (e.key === 'Escape') {
+    // Действие при клике
+    document.querySelector('.header').classList.remove('open');
+  }
 });
 
 // Закрыть меню при клике вне его
-document.getElementById("nav__panel").addEventListener('click', event => {
-    event._isClickWithInMenu = true;
+document.getElementById('nav__panel').addEventListener('click', (event) => {
+  event._isClickWithInMenu = true;
 });
-document.getElementById("burger").addEventListener('click', event => {
-    event._isClickWithInMenu = true;
+document.getElementById('burger').addEventListener('click', (event) => {
+  event._isClickWithInMenu = true;
 });
-document.body.addEventListener('click', event => {
-    if (event._isClickWithInMenu) return;
-    // Действие при клике
-    document.querySelector(".header").classList.remove("open")
+document.body.addEventListener('click', (event) => {
+  if (event._isClickWithInMenu) return;
+  // Действие при клике
+  document.querySelector('.header').classList.remove('open');
 });
 
-// slider
+// slider desktop
+const track = document.querySelector('.track-dt');
+const carouselWidth = document.querySelector(
+  '.carousel-container-dt'
+).offsetWidth;
 
-const slide = document.querySelectorAll('.slide__about');
+document
+  .querySelectorAll('.slider-pagination-dt li')
+  .forEach(function (indicator, index) {
+    indicator.addEventListener('click', function () {
+      articleIndex = index;
+      document
+        .querySelector('.slider-pagination-dt .selected')
+        .classList.remove('selected');
+      indicator.classList.add('selected');
+      track.style.transform = 'translate(' + articleIndex * -20 + '%)';
+    });
+  });
 
+// slider tablet
+
+const slider = document.querySelector('.slider');
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
-const arrow = document.querySelectorAll('.arrow');
-let slidesLength = slide.length;
-let articleIndex = 0;
-let currentIndex = 0;
-let newIndex = 0;
-const mediaQuery = window.matchMedia('(max-width: 1025px)');
 const indicatorParents = document.querySelector('.slider-pagination ul');
-let selectorIndex = 0;
-let paginationElement = document.querySelectorAll(
-  '.pagination-element');
+let articleIndex = 0;
 
+rightArrow.addEventListener('click', function () {
+  articleIndex = articleIndex < 4 ? articleIndex + 1 : 4;
+  document
+    .querySelector('.slider-pagination .selected')
+    .classList.remove('selected');
+  indicatorParents.children[articleIndex].classList.add('selected');
+  slider.style.transform = 'translate(' + articleIndex * -20 + '%)';
+  // slider.style.transform = 'translate(`${articleIndex} * -20`%)';
+});
 
-if (mediaQuery.matches) {
-  function arrowSlider() {
-    if (newIndex === 0) {
-      arrow[0].disabled = true;
-      arrow[0].classList.add('disabled');
-    } else {
-      arrow[0].disabled = false;
-    }
-    if (newIndex === slidesLength - 1) {
-      arrow[1].disabled = true;
-      arrow[1].classList.add('disabled');
-    } else {
-      arrow[1].disabled = false;
-    }
-    slide[newIndex].style.display = 'block';
-    slide[currentIndex].style.display = 'none';
-    currentIndex = newIndex;
-  }
+leftArrow.addEventListener('click', function () {
+  articleIndex = articleIndex > 0 ? articleIndex - 1 : 0;
+  document
+    .querySelector('.slider-pagination .selected')
+    .classList.remove('selected');
+  indicatorParents.children[articleIndex].classList.add('selected');
+  slider.style.transform = 'translate(' + articleIndex * -20 + '%)';
+});
 
-  arrow[0].addEventListener('click', function () {
-    newIndex--;
-    arrowSlider();
+document
+  .querySelectorAll('.slider-pagination li')
+  .forEach(function (indicator, index) {
+    indicator.addEventListener('click', function () {
+      articleIndex = index;
+      document
+        .querySelector('.slider-pagination .selected')
+        .classList.remove('selected');
+      indicator.classList.add('selected');
+      slider.style.transform = 'translate(' + articleIndex * -20 + '%)';
+    });
   });
 
-  arrow[1].addEventListener('click', function () {
-    newIndex++;
-    arrowSlider();
+// faded books-slider
+
+const controls = document.querySelectorAll('.radio__item');
+
+const fadedSlides = document.querySelectorAll('.season-slide');
+
+let currentSlide = 1;
+
+let manualNav = function (manual) {
+  fadedSlides.forEach((slide) => {
+    slide.classList.remove('active');
+
+    controls.forEach((control) => {
+      control.classList.remove('checked');
+    });
   });
 
-  
-    };
+  fadedSlides[manual].classList.add('active');
+  controls[manual].classList.add('checked');
+};
 
+controls.forEach((control, i = 0) => {
+  control.addEventListener('click', () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
 
+// drop-down menu non-registered user
 
+const logo = document.querySelector('.logo');
 
-// pagination
+logo.addEventListener('click', () => {
+  document.getElementById('registration').style.display = 'block';
+});
 
+// non-registered user : registration
 
-   
+// первая маленькая форма
 
+const dropRegister = document.getElementById('drop-register');
 
+dropRegister.addEventListener('click', () => {
+  document.querySelector('.reg-form').style.display = 'block';
+  document.getElementById('registration').style.display = 'none';
+});
 
-// righttArrow.addEventListener('click', function () {
-    
-//       articleIndex = articleIndex < 4 ? articleIndex + 1 : 4;
-//     //   slider.style.transform = 'translate(' + articleIndex * -20 + '%)';
-//     slider.style.transform = `translate(${articleIndex * -100}%)`;
+// при клике на register drop-down registration основная форма регистрации не появляется
+
+// document.body.addEventListener('click', () => {
+//   const regForm = document.querySelector('.reg-form');
+//   console.log(regForm);
+//   if (regForm.style.display === 'block') {
+//     regForm.style.display = 'none';
+//     console.log(regForm);
+//   }
 // });
 
-// leftArrow.addEventListener('click', function () {
-//     articleIndex = (articleIndex > 0) ? articleIndex - 1 : 0;
-//     slider.style.transform = 'translate(' + (articleIndex) * -20 + '%)';
-    
-// })
+// digital card
+const signUpButton = document.getElementById('signUpButton');
+signUpButton.addEventListener('click', () => {
+  document.querySelector('.reg-form').style.display = 'block';
+});
 
-// document
-//   .querySelectorAll('.slider-pagination li')
-//   .forEach(function (indicator, index) {
-//     indicator.addEventListener('click', function () {
-//       articleIndex = index;
-//       document
-//         .querySelector('.slider-pagination .selected')
-//         .classList.remove('selected');
-//       indicator.classList.add('selected');
-//       slide.style.transform = 'translate(' + articleIndex * -20 + '%)';
+const close = document.querySelector('.close');
+close.addEventListener('click', () => {
+  document.querySelector('.reg-form').style.display = 'none';
+  document.querySelector('.registration-modal-content').reset();
+});
+
+const signUp = document.getElementById('sign-up');
+const FORM = document.getElementById('registration-modal-content');
+
+//
+FORM.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // console.log(event);
+
+  FORM.submit();
+});
+
+// const signUp = e => {
+//   let user = JSON.parse(localStorage.getItem('user')) || [];
+//   let exist = user.length && JSON.parse(localStorage.getItem('user')).some(data => data.email == document.getElementById('email').value
+//   );
+
+//   if (!exist) {
+
+//     user.push({
+//       firstName: document.getElementById('first-name').value,
+//       lastName: document.getElementById('last-name').value,
+//       email: document.getElementById('email').value,
+//       pass: document.getElementById('pass').value,
 //     });
-//   });
+//     localStorage.setItem('user', JSON.stringify(user));
+//     document.querySelector('.registration-modal-content').reset();
+//     e.preventDefault();
+//     document.getElementById('first-name').focus();
 
+//   } else {
+//     alert("log in")
+//   }
+// }
 
-// console.log(
-//   '1.Вёрстка соответствует макету. Ширина экрана 768px +26\n2.Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n3.На ширине экрана 768рх реализовано адаптивное меню +12 (Рекомендуется сделать появление бургер-меню на ширине 1024px'
-// );
+// card number
+
+let maxNumber = 999999999;
+const cardNumber = Math.floor(Math.random() * maxNumber + 1);
+console.log(cardNumber);
