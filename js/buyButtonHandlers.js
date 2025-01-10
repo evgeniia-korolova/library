4bf7114f9cd2090a32cbc14efeffd2abb7b295a7
evgeniia-korolova <jkod@ukr.net>
2025-01-10 05:53:51 +0200

    fix buyButtonHandlers messages

--- js/buyButtonHandlers.js
+++ js/buyButtonHandlers.js
@@ -2,15 +2,17 @@ import { showOverlayMessage } from './helpers.js';
 import { openSubscriptionModal } from './subscriptionModal.js';
 import { getCurrentUser } from './helpers.js'; // Предполагается, что эта функция реализована
 
-export function initBuyButtonHandlers() {
+export function initBuyButtonHandlers(currentUser) {
   const buyButtons = document.querySelectorAll('.buy-book-btn');
-  const currentUser = getCurrentUser(); // Получаем текущего пользователя
+  // const currentUser = getCurrentUser(); // Получаем текущего пользователя
 
   buyButtons.forEach((button) => {
     button.addEventListener('click', () => {
       if (!currentUser) {
         showOverlayMessage('Please register and log in to buy a book!');
       } else if (!currentUser.isLoggedIn) {
+        console.log(currentUser);
+        
         showOverlayMessage('Please log in to buy a book!');
       } else {
         openSubscriptionModal();
